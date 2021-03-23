97c3786d7150fc6bd96874af17d68abf4e2b83c6
Mitchell Gritts <mitchellgritts@pm.me>
2021-03-23 10:36:02 -0700

    :tada: Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A quick, interactive map of NDOW hunt and fish license vendors.
+
+## Get started
+
+1. Fork and clone the repo
+2. `npm install` to install dependencies
+3. `npm run dev` to start the dev server
---
+++ index.js
@@ -0,0 +1,28 @@
+import 'ol/ol.css'
+import { Map, View } from 'ol'
+import { useGeographic } from 'ol/proj'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+// makes it so the map view uses geographic coordinates
+useGeographic();
+
+// create a new openlayer map instance
+const map = new Map({
+  // target: means the dom element to put the map in
+  target: 'map',
+
+  // layers: tile layers to use within the map
+  layers: [
+    new TileLayer({
+      // shortcut to specify OpenStreetMaps, instead of needing the url
+      source: new OSM()
+    })
+  ],
+
+  // the initial view of the map
+  view: new View({
+    center: [-117, 39],
+    zoom: 5
+  })
+})
