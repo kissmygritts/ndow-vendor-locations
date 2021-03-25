f3c2029c223b6729857e2f13c47c1a1885fc99cb
iholl <ian.michael.holl@gmail.com>
2021-03-24 17:08:04 -0700

    added basic popup on click fucntionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'ol/ol.css'
-import { Map, View } from 'ol'
-import { useGeographic } from 'ol/proj'
+import { Map, Overlay, View } from 'ol'
+import { toLonLat, useGeographic } from 'ol/proj'
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
 import VectorLayer from 'ol/layer/Vector';
@@ -8,18 +8,40 @@ import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
 
 import data from './data/test_vendor_location_data.geojson';
+import { toStringHDMS } from 'ol/coordinate'
 
 // makes it so the map view uses geographic coordinates
 useGeographic()
 
+//Elements that make up the popup
+let container = document.getElementById('popup');
+let content = document.getElementById('popup-content');
+let closer = document.getElementById('popup-closer');
+
+//Create an overlay to anchor the popup to the map
+const overlay = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+//Add a click handler to hide the popup
+closer.onclick = function() {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false
+};
+
 const test_source =  new VectorSource({
   format: new GeoJSON(),
   url: data,
-})
+});
 
 const test_layer =  new VectorLayer({
   source: test_source,
-})
+});
 
 // create a new openlayer map instance
 const map = new Map({
@@ -35,9 +57,19 @@ const map = new Map({
     }), test_layer,
   ],
 
+  overlays: [overlay],
+
   // the initial view of the map
   view: new View({
     center: [-117, 39],
     zoom: 5
   })
 })
+
+map.on('singleclick', function(evt) {
+  var coordinate = evt.coordinate;
+  var hdms = toStringHDMS((coordinate));
+
+  content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+  overlay.setPosition(coordinate);
+});
