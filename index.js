c8addd45e296217cc3a206d6632ac0fdf377d4ae
iholl <ian.michael.holl@gmail.com>
2021-03-25 16:40:55 -0700

    added geolocation, zoom to location feature, added some styling and comments

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import OSM from 'ol/source/OSM'
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
+import {Fill, Stroke, Style, Circle} from 'ol/style';
+import Feature from 'ol/Feature';
+import {circular} from 'ol/geom/Polygon';
+import Point from 'ol/geom/Point';
+import {fromLonLat} from 'ol/proj';
+import Control from 'ol/control/Control';
 
 import data from './data/vendors.geojson';
 
@@ -33,15 +39,45 @@ closer.onclick = function() {
   return false
 };
 
-// vector data source for vector data layer
-const test_source =  new VectorSource({
+// vendor layer point styling
+var pointStyle = new Style({
+  image: new Circle({
+    radius: 7,
+    fill: new Fill({color: [90, 100, 125, 0.5]}),
+    stroke: new Stroke({
+      color: [53, 85, 166, 0.5], width: 2
+    })
+  })
+})
+
+// vendor location data source for vector data layer
+const vendor_location_source =  new VectorSource({
   format: new GeoJSON(),
   url: data,
 });
 
-// vector data layer
-const test_layer =  new VectorLayer({
-  source: test_source,
+// vendor location data layer
+const vendor_locations_layer =  new VectorLayer({
+  source: vendor_location_source,
+  style: pointStyle,
+});
+
+// geolocation layer point styling
+var pointStyle = new Style({
+  image: new Circle({
+    radius: 7,
+    fill: new Fill({color: [92, 49, 57, 0.90]}),
+    stroke: new Stroke({
+      color: [125, 40, 57, 0.90], width: 2
+    })
+  })
+})
+
+// vector data source and vector data layer for geolocation
+const geolocation_source = new VectorSource();
+const geolocation_layer = new VectorLayer({
+  source: geolocation_source,
+  style: pointStyle
 });
 
 // create a new open layer map instance
@@ -55,7 +91,7 @@ const map = new Map({
     new TileLayer({
       // shortcut to specify OpenStreetMaps, instead of needing the url
       source: new OSM()
-    }), test_layer,
+    }), vendor_locations_layer, geolocation_layer,
   ],
 
   // overlay that controls the popup
@@ -76,9 +112,38 @@ map.on('singleclick', function(evt) {
   if (feature) {
     let coordinates = feature.getGeometry().getCoordinates();
     overlay.setPosition(coordinates);
-    let agent_name = feature.get('AGENT_NAME')
-    let address = feature.get('ADDRESS')
-    let phone = feature.get('PHONE')
+    let agent_name = feature.get('name')
+    let address = feature.get('address')
+    let phone = feature.get('phone')
     content.innerHTML = '<p>Agent Name:</p>' + agent_name + '<p>Address:</p>' + address + '<p>Phone Number:</p>' + phone;
   }
 });
+
+// geolocation to display current location
+navigator.geolocation.watchPosition(function(pos) {
+  const coords = [pos.coords.longitude, pos.coords.latitude];
+  geolocation_source.clear(true);
+  geolocation_source.addFeatures([
+    new Feature(new Point(coords)),
+  ]);
+}, function(error) {
+  alert(`ERROR: ${error.message}`);
+}, {
+  enableHighAccuracy: true
+});
+
+// button to zoom to soure
+const locate = document.createElement('div');
+locate.className = 'ol-control ol-unselectable locate';
+locate.innerHTML = '<button title="Locate Me">◎</button>';
+locate.addEventListener('click', function() {
+  if (!geolocation_source.isEmpty()) {
+    map.getView().fit(geolocation_source.getExtent(), {
+      maxZoom: 14,
+      duration: 500
+    });
+  }
+});
+map.addControl(new Control({
+  element: locate
+}));
