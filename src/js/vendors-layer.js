68f3b2ceef377499de600b55b3f3082fe35242ac
iholl <ian.michael.holl@gmail.com>
2021-03-29 10:47:53 -0700

    fixed closure button, add pos_agent to popup, vector styling for pos_agent

--- src/js/vendors-layer.js
+++ src/js/vendors-layer.js
@@ -4,16 +4,16 @@ import GeoJSON from 'ol/format/GeoJSON'
 import { Style, Circle, Fill, Stroke } from 'ol/style'
 import vendors from '../data/vendors.geojson'
 
-const vendorsStyle = new Style({
-  image: new Circle({
-    radius: 7,
-    fill: new Fill({ color: [90, 100, 125, 0.5] }),
-    stroke: new Stroke({
-      color: [53, 85, 166, 0.5],
-      width: 2
-    })
-  })
-})
+// const vendorsStyle = new Style({
+//   image: new Circle({
+//     radius: 7,
+//     fill: new Fill({ color: [90, 100, 125, 0.5] }),
+//     stroke: new Stroke({
+//       color: [53, 85, 166, 0.5],
+//       width: 2
+//     })
+//   })
+// })
 
 const vendorsSource = new VectorSource({
   format: new GeoJSON(),
@@ -22,7 +22,31 @@ const vendorsSource = new VectorSource({
 
 const vendorsLayer = new VectorLayer({
   source: vendorsSource,
-  style: vendorsStyle
+  style: function (feature) {
+    if (feature.values_.pos_agent === 'FALSE') {
+      return new Style({
+        image: new Circle({
+          radius: 7,
+          fill: new Fill({ color: [240, 81, 89, 0.5] }),
+          stroke: new Stroke({
+            color: [140, 35, 40, 0.5],
+            width: 2
+          })
+        })
+      })
+    } else {
+      return new Style({
+        image: new Circle({
+          radius: 7,
+          fill: new Fill({ color: [90, 100, 125, 0.5] }),
+          stroke: new Stroke({
+            color: [53, 85, 166, 0.5],
+            width: 2
+          })
+        })
+      })
+    }
+  }
 })
 
 vendorsLayer.set('name', 'vendors')
